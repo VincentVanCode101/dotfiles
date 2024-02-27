97dd5f017f220166f4f1da1fccfce37f6a264dbb
Chris <vincentvancode101@gmail.com>
2024-02-27 08:29:04 +0100

    remap leader gs from fugitive to neotree git status

--- nvim/.config/nvim/after/plugin/neo-tree.lua
+++ nvim/.config/nvim/after/plugin/neo-tree.lua
@@ -1,2 +1,3 @@
 vim.api.nvim_set_keymap('n', '<C-n>', ':Neotree toggle<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<leader>gs', ':Neotree float git_status<CR>', {noremap = true, silent = true})
 
