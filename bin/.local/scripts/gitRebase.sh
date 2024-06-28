7c6b1760e09f2d9d63876f7edde90ec26df1c219
Chris <vincentvancode101@gmail.com>
2024-06-28 10:03:01 +0200

    Fr 28. Jun 10:03:01 CEST 2024

---
+++ bin/.local/scripts/gitRebase.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Get the current branch name
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+# Get the number of commits the local branch is ahead of the remote branch
+COMMITS_AHEAD=$(git rev-list --count origin/"$CRRENT_BRANCH"..HEAD)
+
+git rebase -i HEAD~"$COMMITS_AHEAD"
+
