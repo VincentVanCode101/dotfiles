f21ae0f1ef9611cba53e06e9d41f01bc77f80517
Chris <vincentvancode101@gmail.com>
2024-06-28 10:03:57 +0200

    Fr 28. Jun 10:03:57 CEST 2024

--- bin/.local/scripts/gitRebase.sh
+++ bin/.local/scripts/gitRebase.sh
@@ -4,7 +4,7 @@
 CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
 # Get the number of commits the local branch is ahead of the remote branch
-COMMITS_AHEAD=$(git rev-list --count origin/"$CRRENT_BRANCH"..HEAD)
+COMMITS_AHEAD=$(git rev-list --count origin/"$CURRENT_BRANCH"..HEAD)
 
 git rebase -i HEAD~"$COMMITS_AHEAD"
 
