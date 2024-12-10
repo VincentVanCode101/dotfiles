fdcfbfa29fd3b9fb5517f9ff3a45209e00f9d2e2
Chris <vincentvancode101@gmail.com>
2024-12-10 14:55:59 +0100

    Di 10. Dez 14:55:59 CET 2024

--- bin/.local/scripts/gitRebase.sh
+++ bin/.local/scripts/gitRebase.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
-# Get the current branch name
-CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+# Count the number of unpushed commits since origin/HEAD
+commit_count=$(git rev-list --count HEAD ^origin/HEAD)
 
-# Get the number of commits the local branch is ahead of the remote branch
-COMMITS_AHEAD=$(git rev-list --count origin/"$CURRENT_BRANCH"..HEAD)
-
-git rebase -i HEAD~"$COMMITS_AHEAD"
+# Check if there are any commits to rebase
+if [ "$commit_count" -gt 0 ]; then
+  echo "Found $commit_count unpushed commits. Starting interactive rebase..."
+  git rebase -i HEAD~$commit_count
+else
+  echo "No unpushed commits found. Nothing to rebase."
+fi
 
