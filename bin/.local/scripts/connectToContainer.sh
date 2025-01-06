1c669dc7796836f98962248cbacd7b65fd878927
Chris <vincentvancode101@gmail.com>
2025-01-06 10:46:28 +0100

    Mo 6. Jan 10:46:27 CET 2025

--- bin/.local/scripts/connectToContainer.sh
+++ bin/.local/scripts/connectToContainer.sh
@@ -11,6 +11,11 @@ fi
 
 container_id=$(echo "$selected_container" | awk '{print $1}')
 
-
-docker exec -it "$container_id" bash || docker exec -it "$container_id" sh
-
+if docker exec "$container_id" which bash > /dev/null 2>&1; then
+    docker exec -it "$container_id" bash
+elif docker exec "$container_id" which sh > /dev/null 2>&1; then
+    docker exec -it "$container_id" sh
+else
+    echo "Failed to connect to container. Neither bash nor sh is available."
+    exit 1
+fi
