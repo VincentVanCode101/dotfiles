0c1a13aae3c94e95ef972b3a8b7e670f86ba7836
Chris <vincentvancode101@gmail.com>
2025-02-08 08:16:23 +0100

    Sa 8. Feb 08:16:23 CET 2025

--- bin/.local/scripts/notes.sh
+++ bin/.local/scripts/notes.sh
@@ -1,6 +1,6 @@
 NOTE_PATH=$1
 
-mkdir -p "$NOTE_PATH" 
+mkdir -p "$NOTE_PATH"
 
 YEAR=$(date +%Y)
 CALENDAR_WEEK=$(date +%V)
@@ -9,9 +9,8 @@ FILENAME="note_${YEAR}_W${CALENDAR_WEEK}.md"
 
 FILE_PATH="$NOTE_PATH/$FILENAME"
 
-# Check if file already exists to avoid overwriting
 if [ ! -f "$FILE_PATH" ]; then
-    cat <<EOF > "$FILE_PATH"
+  cat <<EOF >"$FILE_PATH"
 ---
 id: note_${YEAR}_W${CALENDAR_WEEK}
 aliases: 
@@ -26,4 +25,3 @@ fi
 GEOMETRY="180x44+0+0"
 
 gnome-terminal --geometry=$GEOMETRY -- bash -c "echo -ne '\033]0;nvim $FILE_PATH\007'; cd $NOTE_PATH && nvim $FILENAME; exec zsh"
-
