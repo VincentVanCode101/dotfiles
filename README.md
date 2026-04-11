1179c1bc3fca0960d9502d9c7d27e6550f73c68e
Chris <vincentvancode101@gmail.com>
2026-04-11 09:52:54 +0200

    feat: adjust lockscreen logic

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 setxkbmap de-chris
+
+Maybe crack a:
+
+sudo systemctl enable nvidia-suspend.service
+sudo systemctl enable nvidia-hibernate.service
+sudo systemctl enable nvidia-resume.service
+
+after installing nvidia drivers
--- bin/.local/scripts/lockAndSuspend.sh
+++ bin/.local/scripts/lockAndSuspend.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 # Lock the screen
-i3lock -i ~/Pictures/lock.jpg
+i3lock -c 000001
 
-# Wait for a moment to give i3lock time to engage
-sleep 3
+sleep 1
 
-# Put the system into standby/suspend
 systemctl suspend
 
